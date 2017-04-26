f73aeffd921cce8079afc206a6f0027d09e6e84d
devjanaprime <dev@primeacademy.io>
2017-04-26 14:55:49 -0500

    basic test of client server interaction working

--- public/scripts/koalaHolla.js
+++ public/scripts/koalaHolla.js
@@ -1,3 +1,17 @@
 $( document ).ready( function(){
   console.log( 'JQ' );
+  getKoalas();
 });
+
+function getKoalas(){
+  // make ajax call to server
+  $.ajax({
+    url: '/koalas',
+    type: 'GET',
+    success: function( response ){
+      console.log( 'back with:', response );
+      // receive list of koalas
+      // update DOM
+    } // end success
+  }); //end ajax
+} // ed func
--- server.js
+++ server.js
@@ -23,3 +23,22 @@ app.get( '/', function( req, res ){
   console.log( 'base url hit' );
   res.sendFile( path.resolve( 'public/views/index.html' ) );
 }); // end base url
+
+app.get( '/koalas', function( req, res ){
+  console.log( '/koalas get hit' );
+
+  var allKoalas = [];
+
+  //// REPLACE WITH DATABASE STUFF LATER
+  var tempKoala = {
+    name: 'Anjeli',
+    sex: 'f',
+    age: 3,
+    transfer : true,
+    notes: 'likes good music'
+  }; // just a test
+  allKoalas.push( tempKoala );
+  /////
+
+  res.send( allKoalas );
+}); //end get koalas
